9e546503c39b762af162711ef1bfba522ec65aa4
Oscar <oscarscouts9@gmail.com>
2023-04-30 12:01:09 -0400

    commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-sign-upform
+Hi there, this is a project based on the Odin Project, using HTML, CSS and a little of JavaScript!
---
+++ app.js
@@ -0,0 +1,17 @@
+
+const alert = document.querySelector(".error-message");
+const password = document.querySelector("#password");
+const confirm = document.querySelector("#confirm");
+
+password.addEventListener("keyup", e => {
+    if(password.value !== confirm.value){
+        alert.style.display = "inline-block";
+    }
+})
+
+confirm.addEventListener("keyup", e => {
+    if(password.value == confirm.value){
+        alert.style.display = "none";
+        document.querySelector('input[type="password"]:focus').style.outlineColor = "green";
+    }
+})
